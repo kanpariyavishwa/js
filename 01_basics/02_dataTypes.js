8490a75a8fbb886e60b64628672edbc09f78a66b
kanpariyavishwa <kanpariyavishwa@gmail.com>
2026-07-10 07:07:23 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";//treat all Js code as newer virsion
+
+//alert(3 + 3) only use in browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+     
+ console.log("vishwa")
+
+
+
+let name ="vishu"//string
+let age = 18
+let isLoggedIn =false //boolean
+let state;
+
+//number=> 2 to power 53
+//bigint use for large number
+// string =>"" ''
+//boolean => true/false
+//null => standalone value
+// undefined =>
+    //symbol => unique
+
+//object
+console.log(typeof undefined);
+console.log(typeof null);
