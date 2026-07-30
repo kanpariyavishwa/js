1895e346c18686a3caf052d1fdc534e9c9e2aa75
kanpariyavishwa <kanpariyavishwa@gmail.com>
2026-07-30 06:36:52 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+//Immediately Involked Function Expressions(IIFE)
+
+
+(function chai(){
+    //named IIFE
+    console.log(`DB CONNECTED`);
+})();
+
+((name)=>{
+    console.log(`DB CONNECTED AUR ${name}`);
+})('vishuuuh')
